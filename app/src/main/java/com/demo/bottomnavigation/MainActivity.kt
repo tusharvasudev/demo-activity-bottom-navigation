58be9e9187d8636195b9da6ec2e98cbb3ec9c08c
Tushar Vasudev <tusharvasudev@outlook.com>
2021-09-17 13:16:11 +0530

    bottom navigation view added

--- app/src/main/java/com/demo/bottomnavigation/MainActivity.kt
+++ app/src/main/java/com/demo/bottomnavigation/MainActivity.kt
@@ -2,10 +2,23 @@ package com.demo.bottomnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
